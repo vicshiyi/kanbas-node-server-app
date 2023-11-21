1dc1c8a038e2a55796c97b6e81dd6a1276039acd
Yi Shi <shiyivic@YideMacBook-Pro.local>
2023-11-20 19:44:18 -0500

    commit 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
 // import session from "express-session";
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 
